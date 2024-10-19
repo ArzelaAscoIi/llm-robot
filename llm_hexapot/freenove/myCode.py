3aa1bb42ccef265983df3408cd7d76e5fbfa5c6e
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2024-10-19 23:40:55 +0200

    fix

--- llm_hexapot/freenove/myCode.py
+++ llm_hexapot/freenove/myCode.py
@@ -1,38 +1,40 @@
-# Import everything in the control module,
-# including functions, classes, variables, and more.
-from llm_hexapot.freenove.Control import *
+# # Import everything in the control module,
+# # including functions, classes, variables, and more.
+# from llm_hexapot.freenove.Control import *
 
-# Creating object 'control' of 'Control' class.
-c = Control()
+# # Creating object 'control' of 'Control' class.
+# c = Control()
 
-# example:
-# data=['CMD_MOVE', '1', '0', '25', '10', '0']
-# Move command:'CMD_MOVE'
-# Gait Mode: "1"
-# Moving direction: x='0',y='25'
-# Delay:'10'
-# Action Mode : '0'   Angleless turn
+# # example:
+# # data=['CMD_MOVE', '1', '0', '25', '10', '0']
+# # Move command:'CMD_MOVE'
+# # Gait Mode: "1"
+# # Moving direction: x='0',y='25'
+# # Delay:'10'
+# # Action Mode : '0'   Angleless turn
 
-# Move forward in action mode 1 and gait mode 1
-for i in range(3):
-    data = ["CMD_MOVE", "1", "0", "35", "10", "0"]
-    c.run(data)
-print("forward")
+# # Move forward in action mode 1 and gait mode 1
+# for i in range(3):
+#     data = ["CMD_MOVE", "1", "0", "35", "10", "0"]
+#     c.run(data)
 
-# Move right in action mode 1 and gait mode 1
-for i in range(3):
-    data = ["CMD_MOVE", "1", "35", "0", "10", "0"]
-    c.run(data)
-print("right")
+# # Move right in action mode 1 and gait mode 1
+# for i in range(3):
+#     data = ["CMD_MOVE", "1", "35", "0", "10", "0"]
+#     c.run(data)
+# # Move backward in action mode 2 and gait mode 2
+# for i in range(3):
+#     data = ["CMD_MOVE", "2", "0", "-35", "10", "10"]
+#     c.run(data)
 
-# Move backward in action mode 2 and gait mode 2
-for i in range(3):
-    data = ["CMD_MOVE", "2", "0", "-35", "10", "10"]
-    c.run(data)
-print("back")
+# # Move right in action mode 2 and gait mode 2
+# for i in range(3):
+#     data = ["CMD_MOVE", "2", "35", "0", "10", "10"]
+#     c.run(data)
 
-# Move right in action mode 2 and gait mode 2
-for i in range(3):
-    data = ["CMD_MOVE", "2", "35", "0", "10", "10"]
-    c.run(data)
-print("right")
+
+from llm_hexapot.service.move import MoveService
+
+
+move_service = MoveService()
+move_service.demonstrate_all_moves()
