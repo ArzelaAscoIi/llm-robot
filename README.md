b82dfd7f1576b68e26e0592fb48a5d9ae99824be
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2024-10-19 14:22:29 +0200

    move

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# llm-robot
+# llm-hexapot
+
+[![PyPI - Version](https://img.shields.io/pypi/v/llm-hexapot.svg)](https://pypi.org/project/llm-hexapot)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/llm-hexapot.svg)](https://pypi.org/project/llm-hexapot)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install llm-hexapot
+```
+
+## License
+
+`llm-hexapot` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ llm_hexapot/check.py
@@ -0,0 +1,28 @@
+from llm_hexapot.freenove.test.led import test_Led
+from llm_hexapot.freenove.test.ultrasonic import test_Ultrasonic
+from llm_hexapot.freenove.test.servo import test_Servo
+from llm_hexapot.freenove.test.adc import test_Adc
+from llm_hexapot.freenove.test.buzzer import test_Buzzer
+
+
+def main():
+    print("Program is starting ... ")
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Parameter error: Please assign the device")
+        exit()
+    if sys.argv[1] == "Led":
+        test_Led()
+    elif sys.argv[1] == "Ultrasonic":
+        test_Ultrasonic()
+    elif sys.argv[1] == "Servo":
+        test_Servo()
+    elif sys.argv[1] == "ADC":
+        test_Adc()
+    elif sys.argv[1] == "Buzzer":
+        test_Buzzer()
+
+
+if __name__ == "__main__":
+    main()
