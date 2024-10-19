8eb5045275d3304972c24d6eab9795a3841a1714
ArzelaAscoIi <kristof.herrmann@rwth-aachen.de>
2024-10-19 23:31:11 +0200

    fix

--- llm_hexapot/freenove/IMU.py
+++ llm_hexapot/freenove/IMU.py
@@ -3,7 +3,7 @@ import time
 import math
 import os
 from llm_hexapot.freenove.Kalman import *
-from llm_hexapot.freenove.mpu6050 import mpu6050
+from mpu6050 import mpu6050
 
 
 class IMU:
--- llm_hexapot/freenove/myCode.py
+++ llm_hexapot/freenove/myCode.py
@@ -1,6 +1,6 @@
 # Import everything in the control module,
 # including functions, classes, variables, and more.
-from Control import *
+from llm_hexapot.freenove.Control import *
 
 # Creating object 'control' of 'Control' class.
 c = Control()
@@ -17,17 +17,22 @@ c = Control()
 for i in range(3):
     data = ["CMD_MOVE", "1", "0", "35", "10", "0"]
     c.run(data)
+print("forward")
 
 # Move right in action mode 1 and gait mode 1
 for i in range(3):
     data = ["CMD_MOVE", "1", "35", "0", "10", "0"]
     c.run(data)
+print("right")
+
 # Move backward in action mode 2 and gait mode 2
 for i in range(3):
     data = ["CMD_MOVE", "2", "0", "-35", "10", "10"]
     c.run(data)
+print("back")
 
 # Move right in action mode 2 and gait mode 2
 for i in range(3):
     data = ["CMD_MOVE", "2", "35", "0", "10", "10"]
     c.run(data)
+print("right")
