16c669e3612ea974a9d5b19ba0274e08f0bb7f3a
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2024-10-27 22:54:06 +0100

    feat

--- dc-component-extras/src/dc_custom_component/robot/tool.py
+++ dc-component-extras/src/dc_custom_component/robot/tool.py
@@ -1,26 +1,21 @@
 from __future__ import annotations
-from typing import Any, Dict
+from typing import Any, Dict, List
 from haystack_experimental.components.tools.openapi import OpenAPITool, LLMProvider
 from haystack import component
+from haystack.dataclasses import ChatMessage
 
-# @component
-# class ChatMessageConverter:
-#     def __init__(self):
-#         pass
 
-#     @component.output_types(parsed_messages=List[ChatMessage])
-#     def run(
-#         self,
-#         query: str,
-#     ):
-#         return {"parsed_messages": [ChatMessage.from_user(query)]}
+@component
+class ChatMessageConverter:
+    def __init__(self):
+        pass
 
-#     def to_dict(self) -> Dict[str, Any]:
-#         return default_to_dict(self)
-
-#     @classmethod
-#     def from_dict(cls, data: Dict[str, Any]) -> ChatMessageConverter:
-#         return cls()
+    @component.output_types(parsed_messages=List[ChatMessage])
+    def run(
+        self,
+        query: str,
+    ):
+        return {"messages": [ChatMessage.from_user(query)]}
 
 
 @component
